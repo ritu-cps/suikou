dcf74198c0ce053837df4b9c29dd28cab213d68b
ritu-cps <ritu@cps.im.dendai.ac.jp>
2020-12-04 16:15:55 +0900

    [add]sensor.ino , relay.ino

---
+++ arduino/relay/relay.ino
@@ -0,0 +1,32 @@
+/**********
+ * ---使用品---
+ * マイコンボード : Arduino uno
+ * リレーモジュール : 16 Relay Module Low Level Trigger
+ * ---ピン配置---
+ * リレーモジュール
+ *  入力ピン16本(ピン番号とリレー番号は一致) : HIGH入力 = リレーOFF, LOW入力 = リレーON
+**********/
+
+#define PIN_RELAY_1 2
+#define PIN_RELAY_2 3
+/*#define PIN_RELAY_3
+#define PIN_RELAY_4
+#define PIN_RELAY_5
+#define PIN_RELAY_6
+#define PIN_RELAY_7
+#define PIN_RELAY_8
+#define PIN_RELAY_9
+#define PIN_RELAY_10
+#define PIN_RELAY_11
+#define PIN_RELAY_12
+#define PIN_RELAY_13
+#define PIN_RELAY_14
+#define PIN_RELAY_15
+#define PIN_RELAY_16*/
+
+void setup(){
+
+}
+void loop(){
+
+}
